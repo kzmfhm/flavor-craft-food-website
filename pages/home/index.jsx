e1817ff78e173f04ea6b8e37da5df63f7d342bae
kzmfhm <khuzaimanazik4@gmail.com>
2023-10-12 16:10:00 +0500

    Creating Home Page

---
+++ pages/home/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../../components/Layout/Header'
+import MainSection from '../../components/Layout/MainSection'
+import Navbar from '../../components/NavBar'
+import Footer from '../../components/Layout/Footer'
+
+const Home = () => {
+  return (
+    <>
+        <Header/>
+        <Navbar/>
+        <MainSection/>
+        <Footer/>
+        </>
+  )
+}
+
+export default Home
