4aac53eb1f606fe4401b51ccf00696aa5bbddeab
kzmfhm <khuzaimanazik4@gmail.com>
2023-10-12 16:07:07 +0500

    Create Header Component

---
+++ components/Layout/Header/index.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+import './style.css'
+const Header = () => {
+  return (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+     <h1 className='header'>
+       Flavor Craft
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
