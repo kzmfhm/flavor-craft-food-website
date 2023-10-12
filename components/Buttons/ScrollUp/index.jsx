e0d8581cb7ebddf015332c78553a3a74834df442
kzmfhm <khuzaimanazik4@gmail.com>
2023-10-12 16:00:23 +0500

    Create Buttons component

--- components/Buttons/ScrollUp/index.jsx
+++ components/Buttons/ScrollUp/index.jsx
@@ -1,1 +1,46 @@
+"use client"
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import ScrollUpImage from '../../../public/svg/scrollup.svg';
+import './style.css'; 
 
+const ScrollUp = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+   
+    const handleScroll = () => {
+     
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      setIsVisible(scrollY > 100);
+    };
+
+
+    window.addEventListener('scroll', handleScroll);
+
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []); 
+
+  const scrollToTop = () => {
+  
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <div>
+      {isVisible && (
+        <button className='scroll-up' onClick={scrollToTop}>
+          <Image src={ScrollUpImage} alt='Scroll Up' />
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default ScrollUp;
