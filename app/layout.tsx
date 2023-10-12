4c9e47abf4c6951e125acbf14af8b920731006bc
kzmfhm <khuzaimanazik4@gmail.com>
2023-10-12 15:58:05 +0500

    Create Next.js App

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import "../styles/globals.css";
+import "../styles/style.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Home from "../pages/home";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Flavor Craft",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className}`}>
+        <Home />
+        {children}
+      </body>
+    </html>
+  );
+}
