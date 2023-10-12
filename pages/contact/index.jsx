724fb160af2081c7381678683786302cef4e53b3
kzmfhm <khuzaimanazik4@gmail.com>
2023-10-12 16:25:23 +0500

    Add terms-&-conditions

---
+++ pages/contact/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Appbar from '../../components/Layout/Appbar'
+import ContactUs from '../../sections/contactus'
+
+const Contact = () => {
+  return (
+    <>
+     <Appbar/>
+      <ContactUs/>
+
+    </>
+  )
+}
+
+export default Contact
--- pages/terms-&-conditions/index.jsx
+++ pages/terms-&-conditions/index.jsx
@@ -1,1 +1,17 @@
+import React from 'react'
+import TermsCoditions from '../../sections/terms-&-conditions'
+import Appbar from '../../components/Layout/Appbar'
+import Footer from '../../components/Layout/Footer'
 
+const TermsCodition = () => {
+  return (
+    <div> 
+      
+        <Appbar/>
+        <TermsCoditions/>
+        <Footer/>
+      </div>
+  )
+}
+
+export default TermsCodition
