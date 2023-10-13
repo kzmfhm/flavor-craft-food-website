28f39db3dd3b2ab13164aed8744be9c5a196d4e8
kzmfhm <khuzaimanazik4@gmail.com>
2023-10-13 18:00:44 +0500

    Remove extra code

--- components/NavBar/index.jsx
+++ components/NavBar/index.jsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from 'next/link';
 import { useState } from 'react';
-import './style.css';
+
 
 const Navbar = () => {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
