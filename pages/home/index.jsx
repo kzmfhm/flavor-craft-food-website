ef52ed523154a2023bf9b1535ec77648602f84ad
kzmfhm <khuzaimanazik4@gmail.com>
2023-10-14 14:25:04 +0500

    Refactor NavBar

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../../components/Layout/Header'
+// import Header from '../../components/Layout/Header'
 import MainSection from '../../components/Layout/MainSection'
 import Navbar from '../../components/NavBar'
 import Footer from '../../components/Layout/Footer'
@@ -7,8 +7,8 @@ import Footer from '../../components/Layout/Footer'
 const Home = () => {
   return (
     <>
-        <Header/>
-        <Navbar/>
+        {/* <Header/> */}
+        < Navbar/>
         <MainSection/>
         <Footer/>
         </>
