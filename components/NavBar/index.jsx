1751ceb8e92cc7a4023ec40eb9dd6e5d414bb060
kzmfhm <khuzaimanazik4@gmail.com>
2023-10-15 09:43:34 +0500

    Reset styling in NavBar

--- components/NavBar/index.jsx
+++ components/NavBar/index.jsx
@@ -19,11 +19,11 @@ const Navbar = () => {
   };
 
   return (
-    <nav className={`w-full h-14 pt-[10px] bg-black-400 flex justify-between items-center md:px-4 ${showMobileMenu ? 'mt-[0px]' : ''}`}>
+    <nav className={`w-full h-14 pt-[10px] bg-black-400 flex justify-between items-center md:px-4 ${showMobileMenu ? 'mt-[0px] ' : ''}`}>
       <div className='top'><Header/></div> 
-       <div className="text-3xl font-bold mt-[20px] sticky top-0">
+       {/* <div className="text-3xl font-bold mt-[20px] sticky top-0">
       
-      </div>
+      </div> */}
       <div className={`md:hidden text-red-400 ${showMobileMenu ? 'hidden' : 'ml-[0px]'}`}>
         <Link className="text-4xl menu-icons" href="#" onClick={toggleMobileMenu}>
         <Image src={openMenu} alt='open' className='open' width={30}></Image>
@@ -32,7 +32,7 @@ const Navbar = () => {
       </div>
       <ul
         className={`font-bold text-red-400 md:flex ${
-          showMobileMenu ? 'grid' : 'hidden'
+          showMobileMenu ? 'grid bg-black' : 'hidden'
         } md:block`}
       >
         <li
@@ -64,7 +64,7 @@ const Navbar = () => {
         </li>
         <li
           className={`mx-[20px] text-xl cursor-pointer transition ease-in-out delay-150 hover:text-white hover:-translate-y-1 hover:scale-110  duration-300 ${
-            showMobileMenu ? 'text-sm mr-[0px]' : ''
+            showMobileMenu ? 'text-sm mr-[0px] ' : ''
           }`}
         >
           <Link href="/contact" className={`md:text-xl md:hover:text-white md:hover:-translate-y-1 md:hover:scale-110 ${showMobileMenu ? '' : ''}`} onClick={closeMobileMenu}>
